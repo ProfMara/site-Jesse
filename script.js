2f99423dffa5dffea669c560b235116dc4246044
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-02-16 20:33:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function mostrarVideo(){
+    document.getElementById("video").style.display = "inline-block";
+    document.getElementById("video-botao").style.display = "none";
+}
+
+function pagar(){
+    window.location = "https://pay.hotmart.com/O13324693E?checkoutMode=10&_ga=2.88032795.202027861.1662704579-2014873601.1662178612&_hi=eyJjaWQiOiIxNjc2MzM2MDUyMjA2NzY5MDM5NTY4ODc5NzEwNzAwIiwiYmlkIjoiMTY3NjMzNjA1MjIwNjc2OTAzOTU2ODg3OTcxMDcwMCIsInNpZCI6ImQ0ODRjZjY1ZTAxYTQ4NDFhNzdiMzNjMDAxODMwNzMxIn0=.1676590218482";
+}
